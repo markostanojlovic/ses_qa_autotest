ea29792b581ac2aff57fae69615aa3d71667a5d0
“markostanojlovic” <mstanojlovic@suse.com>
2017-08-29 16:06:57 +0200

    rgw https deploy test v1

--- ses_qa_scripts/NFS_client_test.sh
+++ ses_qa_scripts/NFS_client_test.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 # This script is testing mount of NFS export with different mount options 
-# If running as root, configure: 
+# USAGE:
+# ./NFS_client_test.sh NFS_IP_ADDRESS
 
-timeout_limit=20    # for 5s timeout, it failes to copy 10MB file *** low performance issue
+[[ -n $1 ]] && NFS_HA_IP=$1 || (echo ERROR: Missing NFS IP. ; exit 1)
+
+timeout_limit=5   
 # LOG_FILE=/tmp/NFS_HA_QA_test_$(date +%H_%M_%S).log
 LOG_FILE=/tmp/NFS_HA_QA_test.log
 > $LOG_FILE
 MOUNT_OPTIONS_FILE=/tmp/mount_options_input_file
 ##### mount options input file #####
-echo "mount.nfs4 -o rw,hard,intr,noatime
+echo "\
+mount.nfs4 -o rw,hard,intr,noatime
 mount.nfs4 -o rw,soft,timeo=20,noatime 
 mount -t nfs 
 mount -t nfs -o rw,sync 
-mount.nfs4 "> $MOUNT_OPTIONS_FILE
+mount.nfs4 " > $MOUNT_OPTIONS_FILE
 ####################################
 date >> $LOG_FILE
-NFS_HA_IP=192.168.100.149
 mount_target="${NFS_HA_IP}:/ /mnt"
-base64 /dev/urandom | head --bytes=10MB > /tmp/random.txt # ne moze da se korisi u skriptama, samo za manuelno testiranje 
+# base64 /dev/urandom | head --bytes=10MB > /tmp/random.txt
+openssl rand -base64 10000000 -out > /tmp/random.txt
 
 function test_command_for_timeout {
   timeout $timeout_limit $command_to_test
   timeout_rc=$?
   if [[ $timeout_rc == 0 ]]
     then
-    echo "INFO: command: [[ $command_to_test ]] finished OK" >> $LOG_FILE
+    echo "INFO: command: [ $command_to_test ] finished OK" >> $LOG_FILE
     else
-      echo "ERROR: command: [[ $command_to_test ]] timed out after $timeout_limit seconds" >> $LOG_FILE; exit 1 
+      echo "ERROR: command: [ $command_to_test ] timed out after $timeout_limit seconds" >> $LOG_FILE; exit 1 
     fi
 }
 
@@ -34,9 +38,10 @@ function test_command_for_timeout {
 mount|grep mnt && umount /mnt -f 
 
 # test ping on a HA IP
-ping -q -c 3 $NFS_HA_IP|grep " 0% packet loss" || ( echo "PING status: *** KO ***" >> $LOG_FILE;exit 1 ); [[ $? == 1 ]] && exit 1
+ping -q -c 3 $NFS_HA_IP|grep " 0% packet loss" || ( echo "PING status: *** KO ***" >> $LOG_FILE;exit 1 )
 echo "PING status: OK " >> $LOG_FILE
 
+# TESTING
 while read mount_options
 do
 # test mount 
